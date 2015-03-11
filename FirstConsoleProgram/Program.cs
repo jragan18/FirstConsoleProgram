6ae7e308f6b8a4d5b99ad10fde8fa88abe21d10b
jragan18 <jragan18@email.phoenix.edu>
2015-03-11 14:30:40 -0400

    Adding Digits
    This adds the digits of the number that is input and displays the sum.

---
+++ FirstConsoleProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleProgram
+{
+    class Program {
+    static void Main(string[] args)
+    {
+        int num, sum = 0, d;
+        Console.WriteLine("Enter a Number : ");
+        num = int.Parse(Console.ReadLine());
+        while (num != 0)
+    {
+        d = num % 10;
+        num = num / 10;
+        sum = sum + d;
+    }
+    Console.WriteLine("Sum of Digits of the Number : "+sum);
+    Console.ReadLine();
+    }
+                  }
+
+    }
